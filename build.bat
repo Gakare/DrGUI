d5b527d55cf93c1c17665e76d6b65928769a2495
Gakare <chiong_patrick@yahoo.com>
2026-04-17 19:02:32 -0700

    Added Evan's path for cl command, so I can build in his machine. Moved and updated build.bat so that only need to call vcvarsall.bat and then run build.bat in the root directory

--- build.bat
+++ build.bat
@@ -1,10 +1,14 @@
 @echo off
 
+:: --- Compile/Link Flags ---
 set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Zo -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -FC -Z7 -DDR_INTERNAL=1
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
 
+:: --- Prep Directories ---
 IF NOT EXIST .\build mkdir .\build
+
 pushd .\build
 REM 64-bit build
 cl %CommonCompilerFlags% ..\src\win32_drgui.cpp /link %CommonLinkerFlags%
 popd
+
--- misc/shell.bat
+++ misc/shell.bat
@@ -1,3 +1,7 @@
 @echo off
 cmd.exe /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set path=C:\Users\Owner\dev\drgui;%path%
+
+:: Evan's cl path
+:: cmd.exe /k "C:\Program Files\Microsoft Visual Studio\18\Insiders\VC\Auxiliary\Build\vcvarsall.bat" x64
+pwd | clip
+REM set path=C:\Users\Owner\dev\drgui;%path%
