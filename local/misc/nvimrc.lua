867fb8b9263d9e43fd773b5a7971b774270d4078
Gakare <chiong_patrick@yahoo.com>
2026-04-20 19:10:25 -0700

    Fixed .gitignore and lost files

---
+++ local/misc/nvimrc.lua
@@ -0,0 +1,4 @@
+vim.opt.makeprg = ".\\build"
+vim.keymap.set("n", "<c-c>", "<cmd>make<cr>")
+vim.keymap.set("n", "<a-e>", "<cmd>copen<cr>")
+vim.opt.colorcolumn = "104"
---
+++ local/misc/shell.bat
@@ -0,0 +1,5 @@
+@echo off
+cmd.exe /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+:: Evan's cl path
+:: cmd.exe /k "C:\Program Files\Microsoft Visual Studio\18\Insiders\VC\Auxiliary\Build\vcvarsall.bat" x64
